41b4f46dbd9b839644255f3116197374e7b9b589
SL <samanthl795@gmail.com>
2021-08-03 15:58:20 -0700

    Add User model

---
+++ seeds/userData.js
@@ -0,0 +1,17 @@
+
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'user1',
+        password: 'password1',
+    },
+    {
+        username: 'user2',
+        password: 'password2',
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
