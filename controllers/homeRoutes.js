25612522840ccfc202af5d7eebce3575f48decc7
SL <samanthl795@gmail.com>
2021-08-03 19:54:54 -0700

    Add sign up page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,4 +9,15 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Route for signing up a new user
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('homepage', {
+            signingUp: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
