f4a9ad60c90bfbf6e4309739c6394ffd9045c896
MPx <hellomariap@gmail.com>
2021-08-04 16:38:17 -0700

    Added trip models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,14 @@
 
 const sequelize = require('../config/connection');
 const seedUsers = require('./userData');
+const seedTrip = require('./tripData');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
 
     // Load user data for testing
     await seedUsers();
-
+    await seedTrip();
     process.exit(0);
 };
 
