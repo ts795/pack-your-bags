30c9cb17e9818dfb810d42106b9bd391724b01d3
SL <samanthl795@gmail.com>
2021-08-04 17:19:56 -0700

    Add view for add trip form

--- controllers/tripRoutes.js
+++ controllers/tripRoutes.js
@@ -9,4 +9,13 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/add', async (req, res) => {
+    try {
+        res.render('addTrip', {
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
