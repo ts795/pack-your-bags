ea5f8d9dc768dc5120dd47536e20ae91ec50cd22
SL <samanthl795@gmail.com>
2021-08-04 17:54:10 -0700

    Add client javascript script to post sign up form data

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+// CREATE new user
+router.post('/', async (req, res) => {
+    // TODO: Fill in
+    res.send(200).json({ message: "Successful login" });
+});
 
 // Login
 router.post('/login', async (req, res) => {
