4be20bce3cd36cba8c62bc76493b23d3336fd47f
MT722 <85648603+MT722@users.noreply.github.com>
2021-08-06 10:18:26 -0700

    modified requestUrl with city variable entered by user

--- public/js/getWeather.js
+++ public/js/getWeather.js
@@ -1,5 +1,6 @@
 function getWeather(){
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?id={city ID}&cnt={cnt}&appid=01edf62c59a57233d6fe7cbdaa44e8a2'
+    var city = 
+    var requestUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=01edf62c59a57233d6fe7cbdaa44e8a2'
 
     fetch(requestUrl)
   .then(function (response) {
@@ -8,14 +9,16 @@ function getWeather(){
   .then(function (data) {
     console.log(data);
 
-      var cityName = document.querySelector('#alcohol');//points to the alcohol name in html
-      var temperature = document.querySelector('#drink-append');//points to the image of the drink
-      var status= document.querySelector('#desc');// points to span id in html
+      var cityName = document.querySelector('#alcohol');
+
+      var temperature = document.querySelector('#drink-append');
+
+      var status= document.querySelector('#desc');
       
       
-      cityName.textContent = data[0].name; 
-      temperature.src=data[0];
-      status.textContent = data[0].description + " Suggested Food Pairings: " + data[0].food_pairing;
+      cityName.textContent = data[0].; 
+      temperature.src=data[0].;
+      status.textContent = data[0].;
 
     
   });
